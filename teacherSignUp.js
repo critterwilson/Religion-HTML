9bfa65d13dde8f60d40696e13dedebb253f8d3cc
critterwilson <wil15090@byui.edu>
2019-04-30 10:54:43 -0600

    adding linford changes and some javascript functionality

--- teacherSignUp.js
+++ teacherSignUp.js
@@ -1,7 +1,22 @@
-var form = document.getElementById("signUpForm");
+var form = document.getElementById("signUpForm").elements;
+
+function linford() {
+	if (form.namedItem("professor").value == "Linford, Kristy"){
+		document.getElementById("linford").style.display = "block";
+		document.getElementById("numCourses").style.display = "none";
+	} else { 
+		document.getElementById("linford").style.display = "none";
+		document.getElementById("numCourses").style.display = "block";
+	}
+}
 
 function courseSignUp() {
-	for (var i = 0; i < form.elements["professor"].value; i++) {
-		console.log(form.elements["professor"].value);
+	for (var i=0; i < 6; i++) {
+		str = "courseSignUp_" + i;
+		if (i < form.namedItem("numCourses").value) {
+			document.getElementById(str).style.display = "block";
+		} else {
+			document.getElementById(str).style.display = "none";
+		}
 	}
 }
