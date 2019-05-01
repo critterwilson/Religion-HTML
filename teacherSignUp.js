543faffeeb0341cf1fa9f7030449c55ad30d81b4
critterwilson <wil15090@byui.edu>
2019-05-01 13:08:51 -0600

    full js functionality that differs between Sister Linford and the Professors Interface

--- teacherSignUp.js
+++ teacherSignUp.js
@@ -1,22 +1,29 @@
 var form = document.getElementById("signUpForm").elements;
 
 function linford() {
-	if (form.namedItem("professor").value == "Linford, Kristy"){
+	if (document.getElementById("userSelect").value == "Linford, Kristy"){
 		document.getElementById("linford").style.display = "block";
-		document.getElementById("numCourses").style.display = "none";
+		document.getElementById("signUpForm").style.display = "none";
 	} else { 
 		document.getElementById("linford").style.display = "none";
-		document.getElementById("numCourses").style.display = "block";
+		document.getElementById("signUpForm").style.display = "block";
 	}
 }
 
 function courseSignUp() {
-	for (var i=0; i < 6; i++) {
+	for (var i = 0; i < 6; i++) {
 		str = "courseSignUp_" + i;
 		if (i < form.namedItem("numCourses").value) {
 			document.getElementById(str).style.display = "block";
 		} else {
 			document.getElementById(str).style.display = "none";
+		}
+
+		if (form.namedItem("numCourses").value < 1) {
+			document.getElementById("professorSubmit").style.display = "none";
+		} else {
+			document.getElementById("professorSubmit").style.display = "block";
+
 		}
 	}
 }
